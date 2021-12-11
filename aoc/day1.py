59588f24e49e9f2b1c9180973b3d83d94aa102e3
Max Simmons <max@mxsmns.net>
2021-12-11 09:49:11 -0800

    day1 part b

--- aoc/day1.py
+++ aoc/day1.py
@@ -1,26 +1,33 @@
+from typing import List
 from typing import Optional
 from typing import Sequence
 
 from aoc.data import get_input
 
 
-def count_increasing_readings(readings: Sequence[float]) -> int:
-    x = readings[0]
+def count_increasing_readings(readings: Sequence[float], window: int = 1) -> int:
     num_increases = 0
-    for y in readings[1:]:
-        if y > x:
+    for i in range(len(readings) - window):
+        x = readings[i : i + window]
+        y = readings[i + 1 : i + 1 + window]
+        if sum(y) > sum(x):
             num_increases += 1
-        x = y
     return num_increases
 
 
-def part_a() -> int:
+def part_a(readings: List[int]) -> int:
     readings = [int(s) for s in get_input(1, "a")]
     return count_increasing_readings(readings)
 
 
+def part_b(readings: List[int]) -> int:
+    return count_increasing_readings(readings, window=3)
+
+
 def main(argv: Optional[Sequence[str]] = None) -> int:
-    print(f"Part A solution: {part_a()}")
+    readings = [int(s) for s in get_input(1, "a")]
+    print(f"Part A solution: {part_a(readings)}")
+    print(f"Part A solution: {part_b(readings)}")
     return 0
 
 
--- tests/day1_test.py
+++ tests/day1_test.py
@@ -28,3 +28,29 @@ from aoc.day1 import count_increasing_readings
 )
 def test_part_a(data, expected):
     assert count_increasing_readings(data) == expected
+
+
+@pytest.mark.parametrize(
+    ("data", "expected"),
+    (
+        pytest.param(
+            [
+                199,
+                200,
+                208,
+                210,
+                200,
+                207,
+                240,
+                269,
+                260,
+                263,
+            ],
+            5,
+            id="example",
+        ),
+        # pytest.param([int(s) for s in get_input(1, "a")], 1692, id="input"),
+    ),
+)
+def test_part_b(data, expected):
+    assert count_increasing_readings(data, window=3) == expected
